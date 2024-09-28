20a468c8960880b1b91b00505e9103f9e38c4be4
Tushar Ahmmed <tushar7425452@gmail.com>
2024-09-28 08:08:46 +0600

    Database Connection Implemented

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-import { PORT } from "./app/config/config.js"
+import { database, PORT } from "./app/config/config.js"
 import express from 'express'
 import router from "./router/api.js"
+import mongoose from "mongoose"
 
 const app = express()
 app.use(router)
+
+
+mongoose.connect(database,{autoIndex:true})
+.then(()=>{
+    console.log(`Database Connected Successfullly....`);
+    
+})
+.catch((error)=>{
+    console.error(error)
+    
+})
 
 app.listen(PORT, ()=>{
     console.log(`server running ar http://localhost:${PORT}`)
--- app/config/config.js
+++ app/config/config.js
@@ -1,2 +1,2 @@
 export const PORT = 5000
-export const database = ""
+export const database = "mongodb+srv://tushar:123@e-commerce.nwu96.mongodb.net/E-Commerce?retryWrites=true&w=majority&appName=E-Commerce"
